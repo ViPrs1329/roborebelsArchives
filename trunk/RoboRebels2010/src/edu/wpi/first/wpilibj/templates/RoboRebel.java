c50e233241d3a10f4f40123ef042e7d7ea3c067c
pydron <pydron@ef8bbffc-0ded-11df-96d9-85c6b80b8d9c>
2010-01-31 01:40:25 +0000

    Test commit.

--- trunk/RoboRebels2010/src/edu/wpi/first/wpilibj/templates/RoboRebel.java
+++ trunk/RoboRebels2010/src/edu/wpi/first/wpilibj/templates/RoboRebel.java
@@ -9,6 +9,9 @@ package edu.wpi.first.wpilibj.templates;
 
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Joystick;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -18,12 +21,35 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * directory.
  */
 public class RoboRebel extends IterativeRobot {
+
+    // Declare variable for the robot drive system
+    RobotDrive m_robotDrive;		// robot will use PWM 1-4 for drive motors
+
+    // Declare a variable to use to access the driver station object
+    DriverStation m_ds;                     // driver station object
+    int m_priorPacketNumber;                // keep track of the most recent packet number from the DS
+    int m_dsPacketsReceivedInCurrentSecond;	// keep track of the ds packets received in the current second
+
+    // Declare variables for the two joysticks being used
+    Joystick m_rightStick;			// joystick 1 (arcade stick or right tank stick)
+    Joystick m_leftStick;			// joystick 2 (tank left stick)
+
+    static final int NUM_JOYSTICK_BUTTONS = 16;
+    boolean[] m_rightStickButtonState = new boolean[(NUM_JOYSTICK_BUTTONS+1)];
+    boolean[] m_leftStickButtonState = new boolean[(NUM_JOYSTICK_BUTTONS+1)];
+
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
-    public void robotInit() {
+    public void robotInit()
+    {
 
+    }
+
+    public void disabledInit()
+    {
+        
     }
 
     public void autonomousInit()
@@ -31,17 +57,32 @@ public class RoboRebel extends IterativeRobot {
         
     }
 
+    public void teleopInit()
+    {
+
+    }
+
     /**
      * This function is called periodically during autonomous
      */
-    public void autonomousPeriodic() {
+    public void autonomousPeriodic()
+    {
 
     }
 
     /**
      * This function is called periodically during operator control
      */
-    public void teleopPeriodic() {
+    public void teleopPeriodic()
+    {
+        
+    }
+
+    /**
+     * This function is called periodically during the disabled state
+     */
+    public void disabledPeriodic()
+    {
         
     }
     
