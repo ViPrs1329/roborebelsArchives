134a5893933248ab9af9d12e08761f01f1f8eb0a
pydron <pydron@ef8bbffc-0ded-11df-96d9-85c6b80b8d9c>
2010-02-15 21:36:58 +0000

    Added pullup object for our pullup device.

---
+++ trunk/RoboRebels2010/src/edu/wpi/first/wpilibj/templates/RRPullup.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.wpi.first.wpilibj.templates;
+
+/*
+ * Controls the pullup device.
+ */
+
+public class RRPullup
+{
+
+}
