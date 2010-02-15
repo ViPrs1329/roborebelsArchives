22ccf2341165ce94e51726df4372ddd91c628f70
pydron <pydron@ef8bbffc-0ded-11df-96d9-85c6b80b8d9c>
2010-02-15 22:23:06 +0000

    Kicker clarity.

--- trunk/RoboRebels2010/src/edu/wpi/first/wpilibj/templates/RRKicker.java
+++ trunk/RoboRebels2010/src/edu/wpi/first/wpilibj/templates/RRKicker.java
@@ -84,10 +84,14 @@ public class RRKicker
      * kicker/compressor has sufficient pressure to execute a kick.  We could use this
      * method trigger a light on the robot or on the console.
      *
+     *
+     *
+     * 
      * QUESTION: Currently, the compressorLoop method is locked in an infinite
      * loop.  Remember that we are using a periodic calling convention for this
      * robot, so, unless this will be put into it's own thread this might not
-     * work.   - Derek Ward
+     * work.  You may want to consider using states like in the spinner or
+     * pullup object.               - Derek Ward
      *
      *
      *
