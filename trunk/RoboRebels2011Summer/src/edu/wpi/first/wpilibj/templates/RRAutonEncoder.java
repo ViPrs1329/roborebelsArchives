a747e65262b1d28ae038e9bbc6eae06d63824362
pydron@gmail.com <pydron@gmail.com@ef8bbffc-0ded-11df-96d9-85c6b80b8d9c>
2011-07-29 19:10:02 +0000

    Started creating the skeleton for the RRAutonEncoder and RRAutonLineSensor classes.  I'll leave the line sensor Auton class for Dennis.

--- trunk/RoboRebels2011Summer/src/edu/wpi/first/wpilibj/templates/RRAutonEncoder.java
+++ trunk/RoboRebels2011Summer/src/edu/wpi/first/wpilibj/templates/RRAutonEncoder.java
@@ -17,15 +17,93 @@ public class RRAutonEncoder
     private     RRMecanumDrive      m_drive;
     private     Encoder             m_encoder;
     private     Timer               m_timer;
+    
+    private     int                 m_encoderChannel1,
+                                    m_encoderChannel2;
+    
+    private     int                 DriveState;
+    
+    private     final int           START = 0,
+                                    STOP = 1;
 
     public RRAutonEncoder(RRMecanumDrive d)
     {
+        m_encoderChannel1 = 1;
+        m_encoderChannel2 = 2;
+        
+        // Only collect the MecanumDrive object
+        // if it's not null
+        if ( d != null )
+            m_drive = d;
 
+        // Set up necessary objects and vars
+        Setup();
+    }
+    
+    
+    public RRAutonEncoder(RRMecanumDrive d, int encChannel1, int encChannel2)
+    {
+        m_encoderChannel1 = encChannel1;
+        m_encoderChannel2 = encChannel2;
+        
+        // Only collect the MecanumDrive object
+        // if it's not null
+        if ( d != null )
+            m_drive = d;
+
+        // Set up necessary objects and vars
+        Setup();
     }
 
     private void Setup()
     {
-
+        SetupTimer();
+        SetupEncoder();
+        reset();
+    }
+    
+    private void SetupTimer()
+    {
+        m_timer = new Timer();
+    }
+    
+    private void SetupEncoder()
+    {
+        m_encoder = new Encoder(m_encoderChannel1, m_encoderChannel2);
     }
 
+    
+    public void init()
+    {
+        m_timer.start();
+    }
+    
+    
+    public void run()
+    {
+        switch ( DriveState )
+        {
+            case START:
+                
+                
+                break;
+                
+            case STOP:
+                
+                
+                break;
+                
+            default:
+                
+                break;
+        }
+    }
+    
+    
+    public void reset()
+    {
+        m_timer.reset();
+        
+        DriveState = START;
+    }
 }
--- trunk/RoboRebels2011Summer/src/edu/wpi/first/wpilibj/templates/RRAutonLineSensor.java
+++ trunk/RoboRebels2011Summer/src/edu/wpi/first/wpilibj/templates/RRAutonLineSensor.java
@@ -5,10 +5,108 @@
 
 package edu.wpi.first.wpilibj.templates;
 
+import edu.wpi.first.wpilibj.Timer;
+
 /**
  *
  * @author Robot-1
  */
-public class RRAutonLineSensor {
+public class RRAutonLineSensor 
+{
+    private     RRMecanumDrive      m_drive;
+    private     RRLineTracker       m_lineTracker;
+    private     Timer               m_timer;
+    
+    private     int                 m_ltChannel1,
+                                    m_ltChannel2,
+                                    m_ltChannel3;
+    
+    private     int                 DriveState;
+    
+    private     final int           START = 0,
+                                    STOP = 1;
+    
+    public RRAutonLineSensor(RRMecanumDrive d)
+    {
+        // default ports for the line tracker is 4, 5, 6
+        m_ltChannel1 = 4;
+        m_ltChannel2 = 5;
+        m_ltChannel3 = 6;
+        
+        // Only collect the MecanumDrive object
+        // if it's not null
+        if ( d != null )
+            m_drive = d;
 
+        // Set up necessary objects and vars
+        Setup();
+    }
+    
+    public RRAutonLineSensor(RRMecanumDrive d, int l1, int l2, int l3)
+    {
+        m_ltChannel1 = l1;
+        m_ltChannel2 = l2;
+        m_ltChannel3 = l3;
+        
+        // Only collect the MecanumDrive object
+        // if it's not null
+        if ( d != null )
+            m_drive = d;
+
+        // Set up necessary objects and vars
+        Setup();
+    }
+    
+    private void Setup()
+    {
+        SetupTimer();
+        SetupLineTracker();
+        reset();
+    }
+    
+    private void SetupTimer()
+    {
+        m_timer = new Timer();
+    }
+    
+    
+    private void SetupLineTracker()
+    {
+        m_lineTracker = new RRLineTracker(m_ltChannel1, m_ltChannel1, m_ltChannel1);
+    }
+
+    
+    public void init()
+    {
+        m_timer.start();
+    }
+    
+    
+    public void run()
+    {
+        switch ( DriveState )
+        {
+            case START:
+                
+                
+                break;
+                
+            case STOP:
+                
+                
+                break;
+                
+            default:
+                
+                break;
+        }
+    }
+    
+    
+    public void reset()
+    {
+        m_timer.reset();
+        
+        DriveState = START;
+    }
 }
