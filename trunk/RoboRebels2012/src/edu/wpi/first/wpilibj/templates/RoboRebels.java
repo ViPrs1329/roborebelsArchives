929800b8ed9f5d2eeda3fa4fc3338431b8973b57
dave@sescleifer.com <dave@sescleifer.com@ef8bbffc-0ded-11df-96d9-85c6b80b8d9c>
2012-01-28 18:54:46 +0000

    Camera code

--- trunk/RoboRebels2012/src/edu/wpi/first/wpilibj/templates/RoboRebels.java
+++ trunk/RoboRebels2012/src/edu/wpi/first/wpilibj/templates/RoboRebels.java
@@ -20,9 +20,9 @@
  * 9 L Stick Click
  */
 /*
-   y4
- x3  b2
-   a1
+y4
+x3  b2
+a1
 
 
  * NOTES:
@@ -36,11 +36,7 @@
  * - Just for the sake of cleanliness, I think that we should
  *   delete the following modules:  RRKicker, RRGRabber, RRPullup and RRSpinner
  */
-
-
-
 package edu.wpi.first.wpilibj.templates;
-
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -50,8 +46,8 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj.camera.AxisCamera;
 import edu.wpi.first.wpilibj.camera.AxisCameraException;
-import edu.wpi.first.wpilibj.image.ColorImage;
-import edu.wpi.first.wpilibj.image.NIVisionException;
+import edu.wpi.first.wpilibj.image.*;
+import edu.wpi.first.wpilibj.image.NIVision.MeasurementType;
 
 
 /**
@@ -63,64 +59,44 @@ import edu.wpi.first.wpilibj.image.NIVisionException;
  */
 public class RoboRebels extends IterativeRobot {
 
-    TrackerDashboard    trackerDashboard = new TrackerDashboard();
-
+    TrackerDashboard trackerDashboard = new TrackerDashboard();
     // Declare a variable to use to access the driver station object
-    DriverStation       m_ds;                   // driver station object
-    DriverStationLCD    m_dsLCD;                // driver station LCD object
-    AxisCamera          cam;                    // camera object
-
-
-
-    double              autonomousStartTime;    // holds the start time for autonomous mode
-
-    Joystick            m_rightStick;		// joystick 1 (arcade stick or right tank stick)
-    Joystick            m_leftStick;		// joystick 2 (tank left stick)
-    Joystick            m_xboxStick;
-
-
-    static final int    NUM_JOYSTICK_BUTTONS = 16;  // how many joystick buttons exist?
-    static boolean      disabledStateBroadcasted = false;
-    static boolean      teleopStateBroadcasted = false;
-    static boolean      autonomousStateBroadcasted = false;
-
-    double              kScoreThreshold = .01;      // used in circle tracking code; default = 0.01
-    double              targetTolerance = 1.0;      // used for target tracking
-    
-
-
-    boolean             foundTarget = false,                // have we found the target yet?
-                        autoDrive = false;                  // are we in autonomous drive mode?
-
-
-    double              lastZValue;                         // last Z value for the dial on the joystick
-    double              robotDriveSensitivity = 0.25;       // sensitivity of the RobotDrive object
-
-    boolean             releasedPin = false;
-    
-    Ultrasonic          ultraSonicSensor;
-    double              distanceInInches = -1;
-
-
-
-
+    DriverStation m_ds;                   // driver station object
+    DriverStationLCD m_dsLCD;                // driver station LCD object
+    AxisCamera cam;                    // camera object
+    CriteriaCollection cc;      // the criteria for doing the particle filter operation
+    double autonomousStartTime;    // holds the start time for autonomous mode
+    Joystick m_rightStick;		// joystick 1 (arcade stick or right tank stick)
+    Joystick m_leftStick;		// joystick 2 (tank left stick)
+    Joystick m_xboxStick;
+    static final int NUM_JOYSTICK_BUTTONS = 16;  // how many joystick buttons exist?
+    static boolean disabledStateBroadcasted = false;
+    static boolean teleopStateBroadcasted = false;
+    static boolean autonomousStateBroadcasted = false;
+    double kScoreThreshold = .01;      // used in circle tracking code; default = 0.01
+    double targetTolerance = 1.0;      // used for target tracking
+    boolean foundTarget = false, // have we found the target yet?
+            autoDrive = false;                  // are we in autonomous drive mode?
+    double lastZValue;                         // last Z value for the dial on the joystick
+    double robotDriveSensitivity = 0.25;       // sensitivity of the RobotDrive object
+    boolean releasedPin = false;
+    Ultrasonic ultraSonicSensor;
+    double distanceInInches = -1;
 
     /**
      * Constructor
      */
-    public void RoboRebels()
-    {
-        System.out.println( "RoboRebels()" );
-      
+    public void RoboRebels() {
+        System.out.println("RoboRebels()");
+
     }
 
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
-    public void robotInit()
-    {
-        System.out.println( "robotInit()" );
+    public void robotInit() {
+        System.out.println("robotInit()");
 
 
         //Watchdog.getInstance().setExpiration(0.75);
@@ -155,25 +131,30 @@ public class RoboRebels extends IterativeRobot {
         m_leftStick = new Joystick(2);
         m_rightStick = new Joystick(3);
         m_xboxStick = new Joystick(1);//TODO test, check if problem is solved
-        
+
         int pingChannel = 0;
         int echoChannel = 0;
-        ultraSonicSensor = new Ultrasonic(pingChannel, echoChannel);
-  
-        System.out.println( "Robot Ready" );
+        try {
+            ultraSonicSensor = new Ultrasonic(pingChannel, echoChannel);
+        } catch (Exception e) {
+            System.out.println("WARNING: UltraSonic Sensor not connected.");
+        }
+
+        cam = AxisCamera.getInstance();  // get an instance ofthe camera
+        cc = new CriteriaCollection();      // create the criteria for the particle filter
+        cc.addCriteria(MeasurementType.IMAQ_MT_BOUNDING_RECT_WIDTH, 30, 400, false);
+        cc.addCriteria(MeasurementType.IMAQ_MT_BOUNDING_RECT_HEIGHT, 40, 400, false);
+
+        System.out.println("Robot Ready");
     }
 
-    public void disabledInit()
-    {
-       
-
+    public void disabledInit() {
         teleopStateBroadcasted = false;
         autonomousStateBroadcasted = false;
     }
 
-    public void autonomousInit()
-    {
-        System.out.println( "autonomousInit()" );
+    public void autonomousInit() {
+        System.out.println("autonomousInit()");
 
         disabledStateBroadcasted = false;
         teleopStateBroadcasted = false;
@@ -182,23 +163,22 @@ public class RoboRebels extends IterativeRobot {
         //autonomousStartTime = Timer.getUsClock();
         autonomousStartTime = Timer.getFPGATimestamp();
 
-        
+
     }
 
-    public void teleopInit()
-    {
-        System.out.println( "teleopInit()" );
+    public void teleopInit() {
+        System.out.println("teleopInit()");
 
         disabledStateBroadcasted = false;
         autonomousStateBroadcasted = false;
 
 //        m_rightStick = new Joystick(2);
 //        m_leftStick = new Joystick(1);
-        
+
         ultraSonicSensor.setEnabled(true);
 
 
-       
+
 
         /* Drive station code */
         m_ds = DriverStation.getInstance();
@@ -211,13 +191,52 @@ public class RoboRebels extends IterativeRobot {
      *
      * Notes:
      *
-     *  
+     *
      *
      */
-    
-    public void autonomousPeriodic()
-    {
-       //nothing right now
+    public void autonomousPeriodic() {
+       try {
+            /**
+             * Do the image capture with the camera and apply the algorithm described above. This
+             * sample will either get images from the camera or from an image file stored in the top
+             * level directory in the flash memory on the cRIO. The file name in this case is "10ft2.jpg"
+             *
+             */
+            ColorImage image = cam.getImage();     // comment if using stored images
+//
+            try {
+                image.write("/hey.jpg");
+            } catch (Exception e) {
+                System.out.println("error saving image");
+            }
+            System.out.println("WROTE IMAGE");
+
+            BinaryImage thresholdImage = image.thresholdRGB(25, 255, 0, 45, 0, 47);   // keep only red objects
+            BinaryImage bigObjectsImage = thresholdImage.removeSmallObjects(false, 2);  // remove small artifacts
+            BinaryImage convexHullImage = bigObjectsImage.convexHull(false);          // fill in occluded rectangles
+            BinaryImage filteredImage = convexHullImage.particleFilter(cc);           // find filled in rectangles
+
+            ParticleAnalysisReport[] reports = filteredImage.getOrderedParticleAnalysisReports();  // get list of results
+            for (int i = 0; i < reports.length; i++) {                                // print results
+                ParticleAnalysisReport r = reports[i];
+                System.out.println("Particle: " + i + ":  Center of mass x: " + r.center_mass_x);
+            }
+            System.out.println(filteredImage.getNumberParticles() + "  " + Timer.getFPGATimestamp());
+
+            /**
+             * all images in Java must be freed after they are used since they are allocated out
+             * of C data structures. Not calling free() will cause the memory to accumulate over
+             * each pass of this loop.
+             */
+
+            filteredImage.free();
+            convexHullImage.free();
+            bigObjectsImage.free();
+            thresholdImage.free();
+            image.free();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     /**
@@ -227,11 +246,8 @@ public class RoboRebels extends IterativeRobot {
      * This is the most important method in this class
      * ---------------------
      */
-
-    public void teleopPeriodic()
-    {
-
-     //nothing right now   
+    public void teleopPeriodic() {
+        //nothing right now
     }
 
     /**
@@ -241,8 +257,7 @@ public class RoboRebels extends IterativeRobot {
      *
      *
      */
-    public void disabledPeriodic()
-    {
+    public void disabledPeriodic() {
         //nothing right now
     }
 
@@ -250,41 +265,32 @@ public class RoboRebels extends IterativeRobot {
      * The VM will try to call this function as often as possible during the autonomous state
      *
      */
-
-    public void autonomousContinuous()
-    {
-       //nothing right now
+    public void autonomousContinuous() {
+        //nothing right now
     }
 
     /**
      * The VM will try to call this function as often as possible during the teleop state
      *
      */
-
-    public void teleopContinuous()
-    {
-         updateDSLCD();
-         if (ultraSonicSensor.isEnabled() && ultraSonicSensor.isRangeValid()) {   
+    public void teleopContinuous() {
+        updateDSLCD();
+        if (ultraSonicSensor.isEnabled() && ultraSonicSensor.isRangeValid()) {
             distanceInInches = ultraSonicSensor.getRangeInches();
-            System.out.println("Ultrasonic sensor:  distance (inches) = "+distanceInInches);
-         }
+            System.out.println("Ultrasonic sensor:  distance (inches) = " + distanceInInches);
+        }
     }
 
     /**
      * The VM will try to call this function as often as possible during the disbabled state
      */
-
-    public void disabledContinuous()
-    {
-        
+    public void disabledContinuous() {
     }
 
     /*
      * This method checks buttons and sets states accordingly
      */
-
-    public void checkButtons()
-    {
+    public void checkButtons() {
         //System.out.println( "checkButtons()" );
 
         /*
@@ -296,118 +302,109 @@ public class RoboRebels extends IterativeRobot {
         System.out.flush();
         System.out.println( "RY: " + m_xboxStick.getRawAxis(5));
         System.out.flush();
-        */
-
+         */
     }
 
- 
-
-
-
-   
-    
-    public void processCamera()
-    {
-    //        System.out.println("processCamera()");
-
-        try
-        {
-            if ( cam.freshImage() )
-            {
+    public void processCamera() {
+        try {
+            if (cam.freshImage()) {
                 ColorImage image = cam.getImage();
+                findTargets(image);
                 image.free();
             }
-        }
-        catch (NIVisionException ex)
-        {
-                ex.printStackTrace();
-        }
-        catch (AxisCameraException ex)
-        {
-                ex.printStackTrace();
+        } catch (NIVisionException ex) {
+            ex.printStackTrace();
+        } catch (AxisCameraException ex) {
+            ex.printStackTrace();
         }
     }
 
-        /*
+    private void findTargets(ColorImage image) {
         try {
-                if (cam.freshImage()) {// && turnController.onTarget()) {
-                    System.out.println("processCamera() - Got a fresh image");
-                    //double gyroAngle = gyro.pidGet();
-                    ColorImage image = cam.getImage();
-                    Thread.yield();
-                    Target[] targets = Target.findCircularTargets(image);
-                    Watchdog.getInstance().feed();
-                    Thread.yield();
-                    image.free();
-                    if (targets.length == 0 || targets[0].m_score < kScoreThreshold)
-                    {
-                        System.out.println("No target found");
-                        Target[] newTargets = new Target[targets.length + 1];
-                        newTargets[0] = new Target();
-                        newTargets[0].m_majorRadius = 0;
-                        newTargets[0].m_minorRadius = 0;
-                        newTargets[0].m_score = 0;
+            BinaryImage masked = image.thresholdHSL(136, 182, 45, 256, 116, 256);
+            ParticleAnalysisReport[] particles = masked.getOrderedParticleAnalysisReports();
 
-                        for (int i = 0; i < targets.length; i++)
-                        {
-                            newTargets[i + 1] = targets[i];
-                        }
+        } catch (NIVisionException ex) {
+            ex.printStackTrace();
+        }
+    }
 
-                        autoDrive = true;
-                        autoTurnDirection = 1.0;  // turn right
-                        foundTarget = false;
+    /*
+    try {
+    if (cam.freshImage()) {// && turnController.onTarget()) {
+    System.out.println("processCamera() - Got a fresh image");
+    //double gyroAngle = gyro.pidGet();
+    ColorImage image = cam.getImage();
+    Thread.yield();
+    Target[] targets = Target.findCircularTargets(image);
+    Watchdog.getInstance().feed();
+    Thread.yield();
+    image.free();
+    if (targets.length == 0 || targets[0].m_score < kScoreThreshold)
+    {
+    System.out.println("No target found");
+    Target[] newTargets = new Target[targets.length + 1];
+    newTargets[0] = new Target();
+    newTargets[0].m_majorRadius = 0;
+    newTargets[0].m_minorRadius = 0;
+    newTargets[0].m_score = 0;
 
-                        trackerDashboard.updateVisionDashboard(0.0, 0.0, 0.0, 0.0, newTargets);
-                    } 
-                    else
-                    {
-                        Watchdog.getInstance().feed();
-                        //System.out.println(targets[0]);
-                        System.out.println("Target Angle: " + targets[0].getHorizontalAngle());
-                        System.out.println("Target tolerance difference: " + (targets[0].getHorizontalAngle() - targetTolerance));
-                        //turnController.setSetpoint(gyroAngle + targets[0].getHorizontalAngle());
-                        //
-                        // Use getHorizontalAngle() to determine if the robot is lined up with
-                        // the target or not.  When it gets close to 0.0 it is lined up.  Use
-                        // a tolerance!!!!
-                        //
+    for (int i = 0; i < targets.length; i++)
+    {
+    newTargets[i + 1] = targets[i];
+    }
 
-                        if ( targets[0].getHorizontalAngle() < targetTolerance && targets[0].getHorizontalAngle() > (-1.0 * targetTolerance) )
-                        {
-                            // if it is inside the target tolerance then stop
-                            System.out.println("processCamera() - Found target.  IT IS CENTERED" );
-                            autoDrive = false;
-                            foundTarget = true;
+    autoDrive = true;
+    autoTurnDirection = 1.0;  // turn right
+    foundTarget = false;
 
-                        }
-                        else
-                        {
-                            // if it is outside the target tolerance then move it
-                            System.out.println("processCamera() - Found target, but it is not centered");
-                            autoDrive = true;
-                            if ( targets[0].getHorizontalAngle() > 0.0 )
-                                autoTurnDirection = 1.0;
-                            else if ( targets[0].getHorizontalAngle() < 0.0 )
-                                autoTurnDirection = -1.0;
-                        }
+    trackerDashboard.updateVisionDashboard(0.0, 0.0, 0.0, 0.0, newTargets);
+    }
+    else
+    {
+    Watchdog.getInstance().feed();
+    //System.out.println(targets[0]);
+    System.out.println("Target Angle: " + targets[0].getHorizontalAngle());
+    System.out.println("Target tolerance difference: " + (targets[0].getHorizontalAngle() - targetTolerance));
+    //turnController.setSetpoint(gyroAngle + targets[0].getHorizontalAngle());
+    //
+    // Use getHorizontalAngle() to determine if the robot is lined up with
+    // the target or not.  When it gets close to 0.0 it is lined up.  Use
+    // a tolerance!!!!
+    //
 
-                        trackerDashboard.updateVisionDashboard(0.0, 0.0, 0.0, targets[0].m_xPos / targets[0].m_xMax, targets);
-                    }
-                }
-            } catch (NIVisionException ex) {
-                ex.printStackTrace();
-            } catch (AxisCameraException ex) {
-                ex.printStackTrace();
-            }
-        */
-    
-    
+    if ( targets[0].getHorizontalAngle() < targetTolerance && targets[0].getHorizontalAngle() > (-1.0 * targetTolerance) )
+    {
+    // if it is inside the target tolerance then stop
+    System.out.println("processCamera() - Found target.  IT IS CENTERED" );
+    autoDrive = false;
+    foundTarget = true;
+
+    }
+    else
+    {
+    // if it is outside the target tolerance then move it
+    System.out.println("processCamera() - Found target, but it is not centered");
+    autoDrive = true;
+    if ( targets[0].getHorizontalAngle() > 0.0 )
+    autoTurnDirection = 1.0;
+    else if ( targets[0].getHorizontalAngle() < 0.0 )
+    autoTurnDirection = -1.0;
+    }
+
+    trackerDashboard.updateVisionDashboard(0.0, 0.0, 0.0, targets[0].m_xPos / targets[0].m_xMax, targets);
+    }
+    }
+    } catch (NIVisionException ex) {
+    ex.printStackTrace();
+    } catch (AxisCameraException ex) {
+    ex.printStackTrace();
+    }
+     */
     /*
      * Sends useful information to the LCD on the DriverStation
      */
-
-    public void updateDSLCD()
-    {
-       //nothing right now
+    public void updateDSLCD() {
+        //nothing right now
     }
 }
