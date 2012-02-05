08c49403bbb19a7373532203b7df72dc22d6e066
alan.b.johnston@gmail.com <alan.b.johnston@gmail.com@ef8bbffc-0ded-11df-96d9-85c6b80b8d9c>
2012-02-05 22:28:46 +0000

    Updates to targeting software to work in daylight, adding angle calculation.

--- trunk/RoboRebels2012/src/edu/wpi/first/wpilibj/templates/RRShooter.java
+++ trunk/RoboRebels2012/src/edu/wpi/first/wpilibj/templates/RRShooter.java
@@ -49,27 +49,55 @@ public class RRShooter
         
     }
     
-    public void determineTrajectory() 
+    static double determineAngle(double distance,int targetID) 
+            
+    // distance is distance to basket in feet
+    // targetID indicates if lower (0), middle (1), or upper (2) is the target
+            
     {
-        double muzzleVelocity = 9; //meters per second
-
-        double gravity = 9.81;  //meters per (second)^2
-        double yLower = .466953;
-        double yMiddle = 1.30515;
-        double yHigher = 2.24485;
-        double xDistance = 3.6576; // distance to base of basket (as if shooting from key)
-        double shooterHeight = 1.016; // meters off the ground
-        double y = yMiddle; // define by asking driver "top, middle, or bottom?"
-        /**
-        double tempSqrtEquationHigher = Math.sqrt((muzzleVelocity*muzzleVelocity)-(2*gravity*muzzleVelocity*muzzleVelocity*yHigher)-(gravity*gravity*xDistance));
-        double tempSqrtEquationMiddle = Math.sqrt((muzzleVelocity*muzzleVelocity)-(2*gravity*muzzleVelocity*muzzleVelocity*yMiddle)-(gravity*gravity*xDistance));
-        double tempSqrtEquationLower = Math.sqrt((muzzleVelocity*muzzleVelocity)-(2*gravity*muzzleVelocity*muzzleVelocity*yLower)-(gravity*gravity*xDistance));
-         **/
-        double tempSqrtEquation = Math.sqrt((muzzleVelocity * muzzleVelocity) - (2 * gravity * muzzleVelocity * muzzleVelocity * y) - (gravity * gravity * xDistance));
-
-        double theta = MathUtils.atan(((muzzleVelocity * muzzleVelocity) + (tempSqrtEquation)) / (gravity * xDistance));
-
-
+     double muzzleVelocity = 9; //meters per second
+     
+     double gravity = 9.81;  //meters per (second)^2
+     double yLower = .466953;
+     double yMiddle = 1.30515;
+     double yHigher = 2.24485;
+     double xDistance = 3.6576; // distance to base of basket (as if shooting from key)
+     
+     xDistance = distance /3.28; // converts feet into meters
+     
+     double shooterHeight = 1.016; // meters off the ground
+     
+     double y; // define by asking driver "top, middle, or bottom?" (BELOW)
+     
+     if (targetID == 0)
+     {
+         y = yLower; 
+                 
+         } 
+     else if (targetID == 1)
+         {
+         y = yMiddle;
+           }
+     else if ( targetID == 2 )
+         {
+         y = yHigher;
+        
+        }
+     else 
+         y = yMiddle;
+     /**
+            double tempSqrtEquationHigher = Math.sqrt((muzzleVelocity*muzzleVelocity)-(2*gravity*muzzleVelocity*muzzleVelocity*yHigher)-(gravity*gravity*xDistance));
+            double tempSqrtEquationMiddle = Math.sqrt((muzzleVelocity*muzzleVelocity)-(2*gravity*muzzleVelocity*muzzleVelocity*yMiddle)-(gravity*gravity*xDistance));
+            double tempSqrtEquationLower = Math.sqrt((muzzleVelocity*muzzleVelocity)-(2*gravity*muzzleVelocity*muzzleVelocity*yLower)-(gravity*gravity*xDistance));
+     **/
+     
+     double tempSqrtEquation = Math.sqrt((muzzleVelocity*muzzleVelocity*muzzleVelocity*muzzleVelocity)-(2*gravity*muzzleVelocity*muzzleVelocity*y)-(gravity*gravity*xDistance*xDistance));
+     
+     double theta = MathUtils.atan(((muzzleVelocity*muzzleVelocity)+(tempSqrtEquation))/(gravity*xDistance));
+     
+     theta = theta * ( 180 / 3.14159265); // converts radians to degreese
+   
+     return(theta);
     }
    
     /**
