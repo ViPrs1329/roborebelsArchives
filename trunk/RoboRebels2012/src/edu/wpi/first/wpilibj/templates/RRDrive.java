dcf7d7ff5072269af891218faa0f821242f61c5f
pydron@gmail.com <pydron@gmail.com@ef8bbffc-0ded-11df-96d9-85c6b80b8d9c>
2012-02-07 00:57:51 +0000

    Needs testing! Transfered drive control to xbox controller

--- trunk/RoboRebels2012/src/edu/wpi/first/wpilibj/templates/RRDrive.java
+++ trunk/RoboRebels2012/src/edu/wpi/first/wpilibj/templates/RRDrive.java
@@ -13,6 +13,7 @@
 package edu.wpi.first.wpilibj.templates;
 
 
+import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
@@ -20,48 +21,140 @@ import edu.wpi.first.wpilibj.Timer;
 
 public class RRDrive
 {
-    RobotDrive m_robotDrive;
-    Joystick m_rightStick;
-    Joystick m_leftStick;
-
-    public RRDrive(RobotDrive robotDrive, Joystick rightStick, Joystick leftStick)
-    {
-        m_robotDrive = robotDrive;
-        m_rightStick = rightStick;
-        m_leftStick = leftStick;
-    }
-
-    public RRDrive(RobotDrive robotDrive)
-    {
-        m_robotDrive = robotDrive;
-        m_rightStick = m_leftStick = null;
-    }
-
-    public RRDrive(RobotDrive robotDrive, Joystick j)
-    {
-        m_robotDrive = robotDrive;
-        m_leftStick = j;
-        m_rightStick = null;
-    }
-
-    public void drive( boolean tankDrive )
-    {
-        
-        System.out.println("RRDrive.drive()");
-        if ( m_rightStick == null || m_leftStick == null )
-            return;
-
-        if ( tankDrive )
-            m_robotDrive.tankDrive(m_leftStick, m_rightStick);
-        else
-            m_robotDrive.arcadeDrive(m_rightStick);
-    }
-
-    public void drive(double speed, double curve)
-    {
-        m_robotDrive.drive(speed, curve);
-    }
-
     
+        boolean arcade;
+        private     Joystick    m_xboxStick;
+        private     Jaguar      frontLeftMotor,
+                                frontRightMotor,
+                                backLeftMotor,
+                                backRightMotor;
+
+//    RobotDrive m_robotDrive;
+//    Joystick m_rightStick;
+//    Joystick m_leftStick;
+//
+//    public RRDrive(RobotDrive robotDrive, Joystick rightStick, Joystick leftStick)
+//    {
+//        m_robotDrive = robotDrive;
+//        m_rightStick = rightStick;
+//        m_leftStick = leftStick;
+//    }
+//
+//    public RRDrive(RobotDrive robotDrive)
+//    {
+//        m_robotDrive = robotDrive;
+//        m_rightStick = m_leftStick = null;
+//    }
+//
+//    public RRDrive(RobotDrive robotDrive, Joystick j)
+//    {
+//        m_robotDrive = robotDrive;
+//        m_leftStick = j;
+//        m_rightStick = null;
+//    }
+//
+//    public void drive( boolean tankDrive )
+//    {
+//        
+//        System.out.println("RRDrive.drive()");
+//        if ( m_rightStick == null || m_leftStick == null )
+//            return;
+//
+//        if ( tankDrive )
+//            m_robotDrive.tankDrive(m_leftStick, m_rightStick);
+//        else
+//            m_robotDrive.arcadeDrive(m_rightStick);
+//    }
+//
+//    public void drive(double speed, double curve)
+//    {
+//        m_robotDrive.drive(speed, curve);
+//    }
+
+    public RRDrive(Joystick joystick){
+           
+            
+    }
+    public void drive(boolean arcade){
+              
+           double l_xVal  = m_xboxStick.getRawAxis(1);
+           double l_yVal  = m_xboxStick.getRawAxis(2);
+
+           double r_xVal  = m_xboxStick.getRawAxis(4);
+           double r_yVal  = m_xboxStick.getRawAxis(5);
+        if(arcade){
+           
+           if (Math.abs(l_xVal) < .13){
+               l_xVal = 0;
+           }
+
+           if (Math.abs(l_yVal)< .13){
+               l_yVal = 0;
+           }
+
+           if (Math.abs(r_xVal) < .13){
+               r_xVal = 0;
+           }
+
+           if (Math.abs(r_yVal)< .13){
+               r_yVal = 0;
+           }
+
+           //Change the range of the joystick values to account for the dead zone
+           if (l_xVal > 0){
+               l_xVal = (l_xVal-.13)/(1-.13);
+           }
+            else if (l_xVal < 0){
+               l_xVal = (l_xVal+.13)/(1-.13);
+            }
+
+            if (l_yVal > 0){
+               l_yVal = (l_yVal-.13)/(1-.13);
+           }
+            else if (l_yVal < 0){
+               l_yVal = (l_yVal+.13)/(1-.13);
+            }
+
+            if (r_xVal > 0){
+               r_xVal = (r_xVal-.13)/(1-.13);
+           }
+            else if (r_xVal < 0){
+               r_xVal = (r_xVal+.13)/(1-.13);
+            }
+
+            if (r_yVal > 0){
+               r_yVal = (r_yVal-.13)/(1-.13);
+           }
+            else if (r_yVal < 0){
+               r_yVal = (r_yVal+.13)/(1-.13);
+            }
+           
+           arcadeDrive(l_yVal, l_xVal); 
+        }else{
+           System.out.println("Tank mode stuff goes here"); 
+        }
+    }
+    public void arcadeDrive(double moveValue, double rotateValue){
+        double rightMotorSpeed;
+        double leftMotorSpeed;
+        
+        if (moveValue > 0.0) {
+            if (rotateValue > 0.0) {
+                leftMotorSpeed = moveValue - rotateValue;
+                rightMotorSpeed = Math.max(moveValue, rotateValue);
+            } else {
+                leftMotorSpeed = Math.max(moveValue, -rotateValue);
+                rightMotorSpeed = moveValue + rotateValue;
+            }
+        } else {
+            if (rotateValue > 0.0) {
+                leftMotorSpeed = -Math.max(-moveValue, rotateValue);
+                rightMotorSpeed = moveValue + rotateValue;
+            } else {
+                leftMotorSpeed = moveValue - rotateValue;
+                rightMotorSpeed = -Math.max(-moveValue, -rotateValue);
+            }
+        }
+    }
     
 }
