4b3817e75bbf0d7eb1fa24a881dbdc3e44317ee9
alan.b.johnston@gmail.com <alan.b.johnston@gmail.com@ef8bbffc-0ded-11df-96d9-85c6b80b8d9c>
2012-03-17 00:42:15 +0000

    Added Autonomous class.  Has pseudo code right now.

---
+++ trunk/RoboRebels2012/src/edu/wpi/first/wpilibj/templates/RRAutonomous.java
@@ -0,0 +1,104 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+/**
+ *
+ * @author aidan/alan
+ */
+public class RRAutonomous {
+    
+    int                         target_selected;
+    RRDIPSwitch                 dipSwitch;
+    private     RRTracker       tracker;
+    private     RRBallSensor    sensor;
+    private     boolean         ball_present = false;
+    private     boolean         delay_shooting  = true;
+    
+    void auton_periodic()
+    {
+        
+        //check dip switches
+        
+        if (dipSwitch.getState(0))
+        {
+            target_selected = RoboRebels.HIGHEST_TARGET;   // Read first DIP Switch
+            System.out.println("COOLIO! We're locked onto the top target! =)");
+        }   
+        
+        else if (dipSwitch.getState(1) && dipSwitch.getState(2))
+        {
+            target_selected = RoboRebels.LOWEST_TARGET;
+            System.out.println("We're locked onto the lowest target! =)");
+        }
+        
+        else if (dipSwitch.getState(1))
+        {
+            target_selected = RoboRebels.LEFT_TARGET;
+            System.out.println("COOLIO! We're locked onto the left target! =)");
+        }
+            
+        else if (dipSwitch.getState(2))
+        {
+            target_selected = RoboRebels.RIGHT_TARGET;
+            System.out.println("COOLIO! We're locked onto the right target! =)");
+        }
+            
+         
+        
+        if (dipSwitch.getState(3))
+        {
+            System.out.println("OK! Let's wait for the other team to shoot first...)");
+            delay_shooting = true;
+        }
+            
+                    
+                    
+        //lock onto correct target
+        
+        tracker.trackTarget(target_selected);
+        
+        //calculate trajectory
+        
+        /*
+         * power shooting motor and move loader belts
+         * check sensors 'is ball launched' 
+         */
+        
+        System.out.println("Ball Sensor: " + sensor.getShootSensor());  // True if ball is there, false if no ball
+        
+        if (sensor.getShootSensor() == true)
+        {
+            ball_present = true; 
+        }
+        
+        else if (sensor.getShootSensor() == false && ball_present == true )
+            
+        {
+            // load motor stop
+            
+            ball_present = false;
+           
+        }
+        
+        }
+        
+        
+        
+        // wait for ball to score while re-calculating trajectory
+        
+        //shoot a second time
+    
+    
+        
+        // drive backwards about 5-10 feet (to be closer than other robots to the balls on the bridge)
+        
+        //EXPLODE
+        
+        
+    }
+    
+    
+
