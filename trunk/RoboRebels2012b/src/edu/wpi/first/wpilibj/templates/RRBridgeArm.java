9873a7163f36afe7458adadd9cb7faeee66fc61e
pydron@gmail.com <pydron@gmail.com@ef8bbffc-0ded-11df-96d9-85c6b80b8d9c>
2012-03-26 23:44:37 +0000

    Going to start 2012 code in another branch.  The goal here is to isolate what's causing 100% CPU utilization into it's own thread with an acceptable priority, as well as making sure that critical functionality is smooth and operable.  We'll grab code from RoboRebels2012 as we move along.

---
+++ trunk/RoboRebels2012b/src/edu/wpi/first/wpilibj/templates/RRBridgeArm.java
@@ -0,0 +1,160 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ *
+ * This class has the following components:
+ * 
+ * - Bridge arm
+ * 
+ * TODO:  
+ * 
+ * 
+ * 
+ * @author dmw
+ */
+public class RRBridgeArm 
+{
+    // Reduced the retraction (UP) speed of the arm by half
+    private final double    ARM_DOWN_SPEED = -1.0;
+    private final double    ARM_UP_SPEED = 0.5;
+    private final double    ARM_DEAD_ZONE = 0.2;
+    
+    private final int       BRIDGE_ARM_LOWER_REQUEST = 0,
+                            BRIDGE_ARM_RAISE_REQUEST = 1,
+                            BRIDGE_ARM_LOWERING = 2,
+                            BRIDGE_ARM_RAISING = 3;
+    
+    private final int       BRIDGE_ARM_LOWER_BUTTON_PRESSED = 4,
+                            BRIDGE_ARM_RAISE_BUTTON_PRESSED = 5;
+    
+    private     int         bav_channel;
+    
+    private     double      armSpeed = 0.0;
+    
+    private     Victor      bridgeArmVictor;
+    
+    
+    /**
+     * Set's up BridgeArm victor
+     * 
+     * @param bavc The Victor controlling the BridgeArm
+     */
+    public RRBridgeArm(int bavc)
+    {
+        bav_channel = bavc;
+        
+        bridgeArmVictor = new Victor(bav_channel);
+    }
+    
+    
+    /**
+     * Joystick Buttons/axis    Action                  This button layout is if the auto ball sensing is not functional
+        
+8                       Bridge arm down 
+9                       Bridge arm up   
+     */
+    
+    private void gatherInputStates()
+    {
+        /*
+         * Old, simplified code which doesn't take into account the shooter state
+         * 
+        // Get Bridge Arm button states
+        if ( js.getRawButton(RRButtonMap.BRIDGE_ARM_DOWN) )
+        {
+            System.out.println("Bridge lower arm");
+            armSpeed = ARM_DOWN_SPEED;
+        }
+        else if ( js.getRawButton(RRButtonMap.BRIDGE_ARM_UP) )
+        {
+            System.out.println("Bridge lower arm");
+            armSpeed = ARM_UP_SPEED;
+        }
+        else if ( !js.getRawButton(RRButtonMap.BRIDGE_ARM_DOWN) && !js.getRawButton(RRButtonMap.BRIDGE_ARM_UP) )
+        {
+            armSpeed = 0.0;
+        }
+        * 
+        */
+        //double      armAxis = RRButtonMap.getActionObject(RRButtonMap.BRIDGE_ARM_DOWN).getAxisState();
+        /*
+        
+        if ( !(armAxis < ARM_DEAD_ZONE && armAxis > (-1.0 * ARM_DEAD_ZONE)) )
+        {
+            if ( armAxis < 0 )
+                armSpeed = armAxis;
+            else
+                armSpeed = armAxis * ARM_UP_SPEED;
+        }
+        else
+        {
+            armSpeed = 0.0;
+        }
+        * 
+        */
+        boolean armDown = RRButtonMap.getActionObject(RRButtonMap.BRIDGE_ARM_DOWN).getButtonState();
+        boolean armUp = RRButtonMap.getActionObject(RRButtonMap.BRIDGE_ARM_UP).getButtonState();
+        if (armDown) {
+            lower();
+        }
+        else if (armUp) {
+            raise();
+        }
+        else {
+            stop();
+        }
+        
+    }
+    
+    
+    public void arm()
+    {
+        gatherInputStates();
+        
+        setBridgeArmSpeeds();
+    }
+    
+    
+    public void armAuton()
+    {
+        setBridgeArmSpeeds();
+    }
+    
+    /**
+     * Lowers the BridgeArm
+     */
+    public void lower()
+    {
+        armSpeed = ARM_DOWN_SPEED;
+    }
+    
+    /**
+     * Raises the BridgeArm
+     */
+    public void raise()
+    {
+        armSpeed = ARM_UP_SPEED;
+    }
+    
+    
+    public void stop()
+    {
+        armSpeed = 0.0;
+    }
+    
+    
+    private void setBridgeArmSpeeds()
+    {
+        //System.out.println("Shooter is not in a valid position yet!");
+        bridgeArmVictor.set(armSpeed);
+    }
+    
+    
+}
---
+++ trunk/RoboRebels2012b/src/edu/wpi/first/wpilibj/templates/RRDrive.java
@@ -0,0 +1,287 @@
+/*
+ * St. Louis Priory School Robotics Club
+ * RoboRebels 2010
+ *
+ * This class implements the robot drive object which is
+ * in charge of encapsulating all driving functionality.
+ *
+ * Maintainer:  David Sescleifer
+ *
+ * ;;;
+ */
+
+package edu.wpi.first.wpilibj.templates;
+
+
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.MotorSafety;
+import edu.wpi.first.wpilibj.MotorSafetyHelper;
+
+
+public class RRDrive implements MotorSafety
+{
+    private     MotorSafetyHelper       motorSafetyHelper;
+    
+    private     boolean                 arcade;
+    
+    private     Jaguar                  leftMotor,
+                                        rightMotor;
+    
+    public static final double          kDefaultExpirationTime = 0.1;
+    
+
+    /**
+     * 
+     * @param js
+     * @param leftChannel
+     * @param rightChannel 
+     */
+    public RRDrive(int leftChannel, int rightChannel)
+    {
+        //System.out.println("RRDrive() " + leftChannel + " " + rightChannel);
+        
+        setupDrive(leftChannel, rightChannel);
+        
+        setupMotorSafety();
+    }
+    
+    
+    /**
+     * 
+     * @param lc
+     * @param rc 
+     */
+    private void setupDrive(int lc, int rc)
+    {
+        leftMotor = new Jaguar(lc);
+        rightMotor = new Jaguar(rc);
+    }
+    
+    
+    /**
+     * 
+     * @param tankDrive 
+     */
+    public void drive(boolean tankDrive)
+    {
+//        double l_xVal  = leftJoystick.getRawAxis(1);
+//        double l_yVal  = leftJoystick.getRawAxis(2);
+//
+//        double r_xVal  = leftJoystick.getRawAxis(4);
+//        double r_yVal  = leftJoystick.getRawAxis(5);
+        
+        RRAction    aoX = RRButtonMap.getActionObject(RRButtonMap.ARCADE_STICK_X),
+                    aoY = RRButtonMap.getActionObject(RRButtonMap.ARCADE_STICK_Y);
+        
+        double l_xVal = aoX.getAxisState();
+        double l_yVal = aoY.getAxisState();
+
+        //System.out.println("drive()");
+           
+        
+        if (Math.abs(l_xVal) < .13)
+        {
+            l_xVal = 0;
+        }
+
+        if (Math.abs(l_yVal) < .13)
+        {
+            l_yVal = 0;
+        }
+
+//        if (Math.abs(r_xVal) < .13)
+//        {
+//            r_xVal = 0;
+//        }
+//
+//        if (Math.abs(r_yVal) < .13)
+//        {
+//            r_yVal = 0;
+//        }
+        
+        
+        
+
+        //Change the range of the joystick values to account for the dead zone
+        if (l_xVal > 0)
+        {
+            l_xVal = (l_xVal-.13)/(1-.13);
+        }
+        else if (l_xVal < 0)
+        {
+            l_xVal = (l_xVal+.13)/(1-.13);
+        }
+
+        if (l_yVal > 0)
+        {
+            l_yVal = (l_yVal-.13)/(1-.13);
+        }
+        else if (l_yVal < 0)
+        {
+            l_yVal = (l_yVal+.13)/(1-.13);
+        }
+
+//        if (r_xVal > 0)
+//        {
+//            r_xVal = (r_xVal-.13)/(1-.13);
+//        }
+//        else if (r_xVal < 0)
+//        {
+//            r_xVal = (r_xVal+.13)/(1-.13);
+//        }
+//
+//        if (r_yVal > 0)
+//        {
+//            r_yVal = (r_yVal-.13)/(1-.13);
+//        }
+//        else if (r_yVal < 0)
+//        {
+//            r_yVal = (r_yVal+.13)/(1-.13);
+//        }
+           
+        
+        if (!tankDrive)
+        {
+           arcadeDrive(l_yVal, l_xVal); 
+        }
+        else
+        {
+           //tank mode
+     //       System.out.println("RRDrive::drive() - Tank drive has not been implimented yet!!!");
+        }
+    }
+    
+    
+    /**
+     * 
+     * @param moveValue
+     * @param rotateValue 
+     */
+    public void arcadeDrive(double moveValue, double rotateValue)
+    {
+        double rightMotorSpeed;
+        double leftMotorSpeed;
+        
+        if (moveValue > 0.0) 
+        {
+            if (rotateValue > 0.0) 
+            {
+                leftMotorSpeed = moveValue - rotateValue;
+                rightMotorSpeed = Math.max(moveValue, rotateValue);
+            } 
+            else 
+            {
+                leftMotorSpeed = Math.max(moveValue, -rotateValue);
+                rightMotorSpeed = moveValue + rotateValue;
+            }
+        } 
+        else 
+        {
+            if (rotateValue > 0.0) 
+            {
+                leftMotorSpeed = -Math.max(-moveValue, rotateValue);
+                rightMotorSpeed = moveValue + rotateValue;
+            } 
+            else 
+            {
+                leftMotorSpeed = moveValue - rotateValue;
+                rightMotorSpeed = -Math.max(-moveValue, -rotateValue);
+            }
+        }
+        
+  //      System.out.println("%%%% arcadeDrive() mv: " + RRTracker.round2(moveValue) + " rv: " + RRTracker.round2(rotateValue) + " lms: " + RRTracker.round2(leftMotorSpeed) + " rms: " + RRTracker.round2(rightMotorSpeed));
+        
+        //System.out.println("Left Motor Speed: " + leftMotorSpeed + "RMS: " + rightMotorSpeed);
+        setLeftRightMotorValue(leftMotorSpeed, rightMotorSpeed);
+    }
+    
+    
+    /**
+     * 
+     * @param left
+     * @param right 
+     */
+    public void setLeftRightMotorValue(double left, double right)
+    {
+        if (leftMotor == null || rightMotor == null)
+            throw new NullPointerException("Null motor provided");
+        leftMotor.set(left);
+        rightMotor.set(right);
+        
+        motorSafetyHelper.feed();
+    }
+
+    /**
+     * 
+     * @param timeout 
+     */
+    public void setExpiration(double timeout) {
+        motorSafetyHelper.setExpiration(timeout);
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    public double getExpiration() {
+        return motorSafetyHelper.getExpiration();
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    public boolean isAlive() {
+        return motorSafetyHelper.isAlive();
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    public boolean isSafetyEnabled() {
+        return motorSafetyHelper.isSafetyEnabled();
+    }
+
+    /**
+     * 
+     * @param enabled 
+     */
+    public void setSafetyEnabled(boolean enabled) {
+        motorSafetyHelper.setSafetyEnabled(enabled);
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public String getDescription() {
+        return "RR Robot Drive";
+    }
+
+    /**
+     * 
+     */
+    public void stopMotor() 
+    {
+        if (leftMotor != null) {
+            leftMotor.set(0.0);
+        }
+        if (rightMotor != null) {
+            rightMotor.set(0.0);
+        }
+        
+    }
+    
+    
+    /**
+     * 
+     */
+    private void setupMotorSafety() 
+    {
+        motorSafetyHelper = new MotorSafetyHelper(this);
+        motorSafetyHelper.setExpiration(kDefaultExpirationTime);
+        motorSafetyHelper.setSafetyEnabled(false);
+    }
+}
