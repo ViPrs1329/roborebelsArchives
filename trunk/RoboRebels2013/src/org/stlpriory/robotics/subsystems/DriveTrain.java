1bb0e95b131c708f32d845b76ab0abddfceaf550
dennis.fuglsang@oracle.com <dennis.fuglsang@oracle.com@ef8bbffc-0ded-11df-96d9-85c6b80b8d9c>
2013-01-20 19:07:13 +0000

    RoboRebels 2013 robot control system

--- trunk/RoboRebels2013/src/org/stlpriory/robotics/subsystems/DriveTrain.java
+++ trunk/RoboRebels2013/src/org/stlpriory/robotics/subsystems/DriveTrain.java
@@ -20,21 +20,27 @@ import org.stlpriory.robotics.misc.Debug;
 public class DriveTrain extends Subsystem implements Constants {
 
     private RobotDrive drive;
-    private Jaguar leftJag, rightJag;
+    private Jaguar leftFrontJag;
+    private Jaguar rightFrontJag;
+    private Jaguar leftRearJag;
+    private Jaguar rightRearJag;
     private static double direction = 1;
 
     public DriveTrain() {
         super("DriveTrain");
-        Debug.println("[DriveTrain] Initializing left jaguar on channel " + RobotMap.DRIVE_LEFT_MOTOR);
-        leftJag = new Jaguar(RobotMap.DRIVE_LEFT_MOTOR);
-        Debug.println("[DriveTrain] Initializing right jaguar on channel " + RobotMap.DRIVE_RIGHT_MOTOR);
-        rightJag = new Jaguar(RobotMap.DRIVE_RIGHT_MOTOR);
+        Debug.println("[DriveTrain] Initializing left front jaguar on channel " + RobotMap.DRIVE_FRONT_LEFT_MOTOR);
+        leftFrontJag = new Jaguar(RobotMap.DRIVE_FRONT_LEFT_MOTOR);
+        Debug.println("[DriveTrain] Initializing right front jaguar on channel " + RobotMap.DRIVE_FRONT_RIGHT_MOTOR);
+        rightFrontJag = new Jaguar(RobotMap.DRIVE_FRONT_RIGHT_MOTOR);
 
-        drive = new RobotDrive(leftJag, rightJag);
-        //drive = new RobotDrive(SpeedController frontLeftMotor, SpeedController rearLeftMotor,
-        //    SpeedController frontRightMotor, SpeedController rearRightMotor);
-        drive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
-        drive.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
+        Debug.println("[DriveTrain] Initializing left rear jaguar on channel " + RobotMap.DRIVE_BACK_LEFT_MOTOR);
+        leftRearJag = new Jaguar(RobotMap.DRIVE_BACK_LEFT_MOTOR);
+        Debug.println("[DriveTrain] Initializing right rear jaguar on channel " + RobotMap.DRIVE_BACK_RIGHT_MOTOR);
+        rightRearJag = new Jaguar(RobotMap.DRIVE_BACK_RIGHT_MOTOR);
+
+        drive = new RobotDrive(leftFrontJag, leftRearJag,rightFrontJag, rightRearJag);
+        //drive.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
+        //drive.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
         drive.setSafetyEnabled(false);
     }
 
@@ -74,6 +80,28 @@ public class DriveTrain extends Subsystem implements Constants {
         rotateValue *= direction;
         if (canDrive()) {
             drive.arcadeDrive(moveValue, rotateValue);
+        }
+    }
+
+   /**
+     * Drive method for Mecanum wheeled robots.
+     *
+     * A method for driving with Mecanum wheeled robots. There are 4 wheels
+     * on the robot, arranged so that the front and back wheels are toed in 45 degrees.
+     * When looking at the wheels from the top, the roller axles should form an X across the robot.
+     *
+     * This is designed to be directly driven by joystick axes.
+     *
+     * @param xSpeed The speed that the robot should drive in the X direction. [-1.0..1.0]
+     * @param ySpeed The speed that the robot should drive in the Y direction.
+     * This input is inverted to match the forward == -1.0 that joysticks produce. [-1.0..1.0]
+     * @param rotation The rate of rotation for the robot that is completely independent of
+     * the translation. [-1.0..1.0]
+     * @param gyroAngle The current angle reading from the gyro.  Use this to implement field-oriented controls.
+     */
+    public void mecanumDrive(double xSpeed, double ySpeed, double rotation, double gyroAngle) {
+        if (canDrive()) {
+            drive.mecanumDrive_Cartesian(xSpeed, ySpeed, rotation, gyroAngle);
         }
     }
 
