8eaadb5f13d9a6de3f1244898fc69938d4e63f1f
dennis.fuglsang@oracle.com <dennis.fuglsang@oracle.com@ef8bbffc-0ded-11df-96d9-85c6b80b8d9c>
2013-01-24 02:23:23 +0000

    RoboRebels 2013 robot control system

--- trunk/RoboRebels2013/src/org/stlpriory/robotics/commands/shooter/LoadDisc.java
+++ trunk/RoboRebels2013/src/org/stlpriory/robotics/commands/shooter/LoadDisc.java
@@ -32,6 +32,7 @@ public class LoadDisc extends CommandBase {
      * Called repeatedly when this Command is scheduled to run
      */
     protected void execute() {
+        Debug.println("LoadDisc.execute()");
         shooter.loadDisc();
     }
 
