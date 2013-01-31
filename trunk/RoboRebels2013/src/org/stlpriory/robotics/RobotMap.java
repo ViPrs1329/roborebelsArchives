2f39d03ae43ba36106f739c643fcf68067289d87
dennis.fuglsang@oracle.com <dennis.fuglsang@oracle.com@ef8bbffc-0ded-11df-96d9-85c6b80b8d9c>
2013-01-31 21:55:05 +0000

    RoboRebels 2013 robot control system - code cleanup

--- trunk/RoboRebels2013/src/org/stlpriory/robotics/RobotMap.java
+++ trunk/RoboRebels2013/src/org/stlpriory/robotics/RobotMap.java
@@ -17,26 +17,20 @@ public class RobotMap {
     // public static final int rangefinderPort = 1;
     // public static final int rangefinderModule = 1;
 
-    // Joysticks
-    public static final int DRIVER_STATION_USB_PORT1 = 1;	// USB Port 1 on Driver station PC
-    public static final int DRIVER_STATION_USB_PORT2 = 2;	// USB Port 2 on Driver station PC
-    public static final int DRIVER_STATION_USB_PORT3 = 3;	// USB Port 3 on Driver station PC
-    public static final int DRIVER_STATION_USB_PORT4 = 4;	// USB Port 4 on Driver station PC
+    // Joysticks/gampads mappings to USB ports on the Driver station PC
+    public static final int DRIVER_STATION_USB_PORT1 = 1;  // USB Port
+    public static final int DRIVER_STATION_USB_PORT2 = 2;  // USB Port
+    public static final int DRIVER_STATION_USB_PORT3 = 3;  // USB Port
+    public static final int DRIVER_STATION_USB_PORT4 = 4;  // USB Port
 
-    // Motors/sensors used by the shooter
-    public static final int SHOOTER_SERVO_PWM_CHANNEL  = 5;	// PWM OUT Port 1
-    public static final int SHOOTER_VICTOR_PWM_CHANNEL = 6;	// PWM OUT Port 1
+    // Drive train speed controller mappings to PWM channels on the digital sidecar module
+    public static final int LEFT_FRONT_DRIVE_MOTOR_PWM_CHANNEL  = 2;  // PWM channel
+    public static final int LEFT_REAR_DRIVE_MOTOR_PWM_CHANNEL   = 1;  // PWM channel
+    public static final int RIGHT_FRONT_DRIVE_MOTOR_PWM_CHANNEL = 3;  // PWM channel
+    public static final int RIGHT_REAR_DRIVE_MOTOR_PWM_CHANNEL  = 4;  // PWM channel
 
-    // Drive motors (4 motor mechanum drive)
-    public static final int LEFT_FRONT_DRIVE_MOTOR_PWM_CHANNEL  = 2;	// PWM 1
-    public static final int LEFT_REAR_DRIVE_MOTOR_PWM_CHANNEL   = 1;	// PWM 3
-    public static final int RIGHT_FRONT_DRIVE_MOTOR_PWM_CHANNEL = 3;	// PWM 2
-    public static final int RIGHT_REAR_DRIVE_MOTOR_PWM_CHANNEL  = 4;	// PWM 4
-
-    // Sonar slot
-//    public static final int kSonarChannel = 2;	// Analog 2
-
-    // Gyroscope slot
-//    public static final int kGyroChannel = 1;	// Analog 1
+    // Shooter speed controller and server mappings to PWM channels on the digital sidecar module
+    public static final int SHOOTER_SERVO_PWM_CHANNEL  = 5;	 // PWM channel
+    public static final int SHOOTER_VICTOR_PWM_CHANNEL = 6;	 // PWM channel
 
 }
