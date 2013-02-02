aefadc4bf88e5eca0bf92763932946fa4c6450d2
dennis.fuglsang@oracle.com <dennis.fuglsang@oracle.com@ef8bbffc-0ded-11df-96d9-85c6b80b8d9c>
2013-02-02 21:58:37 +0000

    RoboRebels 2013 robot control system - code cleanup

--- trunk/RoboRebels2013/src/org/stlpriory/robotics/commands/shooter/ShootDisc.java
+++ trunk/RoboRebels2013/src/org/stlpriory/robotics/commands/shooter/ShootDisc.java
@@ -6,31 +6,39 @@ package org.stlpriory.robotics.commands.shooter;
 
 import org.stlpriory.robotics.commands.CommandBase;
 import org.stlpriory.robotics.misc.Constants;
+import org.stlpriory.robotics.misc.Debug;
 
 /**
  *
- * @author dfuglsan
  */
 public class ShootDisc extends CommandBase {
-    
+
+    private double timeout;
+
     public ShootDisc() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+        this(0.5);
+    }
+
+    public ShootDisc(double timeout) {
+        requires(shooter);
+        this.timeout = timeout;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+        setTimeout(timeout);
+        Debug.print("[" + getName() + "]");
+        Debug.print("\tTimeout: " + timeout);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-        System.out.println("Is shooting");
-        shooter.startShooter(Constants.SHOOTER_SPEED);
+       shooter.startShooter(Constants.SHOOTER_SPEED);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return isTimedOut();
     }
 
     // Called once after isFinished returns true
