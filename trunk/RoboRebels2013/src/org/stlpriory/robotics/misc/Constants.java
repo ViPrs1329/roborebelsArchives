7bc347181a26d7bc18bb71d8f34253e03174a2eb
dennis.fuglsang@oracle.com <dennis.fuglsang@oracle.com@ef8bbffc-0ded-11df-96d9-85c6b80b8d9c>
2013-02-02 21:59:12 +0000

    RoboRebels 2013 robot control system - code cleanup

--- trunk/RoboRebels2013/src/org/stlpriory/robotics/misc/Constants.java
+++ trunk/RoboRebels2013/src/org/stlpriory/robotics/misc/Constants.java
@@ -23,6 +23,6 @@ public class Constants {
      */
     public static final double MIN_LOADER_SERVO_ANGLE = -45.0;
     public static final double MAX_LOADER_SERVO_ANGLE = 65.0;
-    public static final double SHOOTER_SPEED = .7;
 
+    public static final double SHOOTER_SPEED = 0.7;
 }
