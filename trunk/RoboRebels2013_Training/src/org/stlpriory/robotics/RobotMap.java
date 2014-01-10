3f8258034246d14c21f786e38f5d2d5a40d7da2e
teststudent@stlprioryschool.org <teststudent@stlprioryschool.org@ef8bbffc-0ded-11df-96d9-85c6b80b8d9c>
2014-01-10 19:36:20 +0000

    Started on new drive base, since we lost the code for what's currently on the SimpleBot.

--- trunk/RoboRebels2013_Training/src/org/stlpriory/robotics/RobotMap.java
+++ trunk/RoboRebels2013_Training/src/org/stlpriory/robotics/RobotMap.java
@@ -6,6 +6,8 @@ package org.stlpriory.robotics;
  * the wiring easier and significantly reduces the number of magic numbers
  * floating around.
  */
+
+
 public class RobotMap {
     // For example to map the left and right motors, you could define the
     // following variables to use with your drivetrain subsystem.
