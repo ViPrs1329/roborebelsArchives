221924cae138f7c50e4ff77df117dbdbe34b3a6f
pydron@gmail.com <pydron@gmail.com@ef8bbffc-0ded-11df-96d9-85c6b80b8d9c>
2014-01-10 19:24:14 +0000

    Started on new drive base, since we lost the code for what's currently on the SimpleBot.

--- trunk/RoboRebels2013_Training/src/org/stlpriory/robotics/OI.java
+++ trunk/RoboRebels2013_Training/src/org/stlpriory/robotics/OI.java
@@ -15,6 +15,8 @@ import org.stlpriory.robotics.misc.Debug;
 
 
 
+
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
