d279e14ca5cc7842de7a73a8d87d06cf4536f2b3
dward@stlprioryschool.org <dward@stlprioryschool.org@ef8bbffc-0ded-11df-96d9-85c6b80b8d9c>
2014-01-19 23:04:42 +0000

    Fixing things

--- trunk/SimpleBot/src/edu/wpi/first/wpilibj/templates/RRDrive.java
+++ trunk/SimpleBot/src/edu/wpi/first/wpilibj/templates/RRDrive.java
@@ -13,75 +13,24 @@
 package edu.wpi.first.wpilibj.templates;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Solenoid;
-
-/**
- * The buttons on the controller follow this mapping
- *
- * 1: A
- * 2: B
- * 3: X
- * 4: Y
- * 5: Left Bumper
- * 6: Right Bumper
- * 7: Back
- * 8: Start
- * 9: Left Joystick
- * 10: Right Joystick
- *
- * The axis on the controller follow this mapping (all output is between -1 and 1)
- *
- * 1: Left Stick X Axis -Left:Negative ; Right: Positive
- * 2: Left Stick Y Axis -Up: Negative ; Down: Positive
- * 3: Triggers -Left: Positive ; Right: Negative
- * 4: Right Stick X Axis -Left: Negative ; Right: Positive
- * 5: Right Stick Y Axis -Up:   Negative ; Down: Positive
- * 6: Directional Pad (Not recommended, buggy)
- */
+import edu.wpi.first.wpilibj.Timer;
 
 
 public class RRDrive
 {
     RobotDrive m_robotDrive;
     Joystick m_xboxController;
-    Solenoid leftSol, rightSol;
-    
-    boolean     m_highGear;
-    boolean     m_gearSwitched;
 
-    public RRDrive(RobotDrive robotDrive, Joystick j, Solenoid ls, Solenoid rs)
+    public RRDrive(RobotDrive robotDrive, Joystick j)
     {
         m_robotDrive = robotDrive;
         m_xboxController = j;
-        m_highGear = false;
-        m_gearSwitched = false;
-        leftSol = ls;
-        rightSol = rs;
     }
 
     public void drive( boolean tankDrive )
     {
         if ( m_xboxController == null )
             return;
-        
-        if (m_xboxController.getRawButton(3) && !m_gearSwitched)
-        {
-            if (m_highGear)
-                m_highGear = false;
-            else
-                m_highGear = true;
-            
-            m_gearSwitched = true;
-        }
-        else if (!m_xboxController.getRawButton(3))
-        {
-            m_gearSwitched = false;
-        }
-        
-        if (m_highGear)
-            engadgeHighGear();
-        else
-            engadgeLowGear();
 
         if ( tankDrive )
             //m_robotDrive.tankDrive(m_leftStick, m_rightStick);\
@@ -93,13 +42,5 @@ public class RRDrive
     public void drive(double speed, double curve)
     {
         m_robotDrive.drive(speed, curve);
-    }
-
-    private void engadgeHighGear() {
-        
-    }
-
-    private void engadgeLowGear() {
-        
     }
 }
