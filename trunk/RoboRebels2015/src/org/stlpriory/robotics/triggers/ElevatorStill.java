dee06926b4d8c192e0ce7b5d0372092a8c3b7f0e
kosterw@stlprioryschool.org <kosterw@stlprioryschool.org@ef8bbffc-0ded-11df-96d9-85c6b80b8d9c>
2015-03-05 00:21:26 +0000

    Re-enabled elevator pulsing.

--- trunk/RoboRebels2015/src/org/stlpriory/robotics/triggers/ElevatorStill.java
+++ trunk/RoboRebels2015/src/org/stlpriory/robotics/triggers/ElevatorStill.java
@@ -9,19 +9,18 @@ import edu.wpi.first.wpilibj.buttons.Trigger;
  *
  */
 
-
 public class ElevatorStill extends Trigger {
 
 	private Joystick j;
-	
+
 	public ElevatorStill(Joystick j) {
 		super();
 		this.j = j;
 	}
-	
-    public boolean get() {
-    	boolean b1 =j.getRawButton(Keymap.ELEVATOR_DOWN_BUTTON_KEY_MAP);
-    	boolean b2 = j.getRawButton(Keymap.ELEVATOR_UP_BUTTON_KEY_MAP);
-        return (!b1&&!b2);
-    }
+
+	public boolean get() {
+		boolean b1 = j.getRawButton(Keymap.ELEVATOR_DOWN_BUTTON_KEY_MAP);
+		boolean b2 = j.getRawButton(Keymap.ELEVATOR_UP_BUTTON_KEY_MAP);
+		return (!b1 && !b2);
+	}
 }
